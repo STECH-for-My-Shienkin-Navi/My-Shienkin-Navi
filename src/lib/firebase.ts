a85fa55a15016123321684be69c8a5f8b09237ff
Yuma Satake <satous.y@icloud.com>
2023-05-17 02:56:15 +0900

    fix: デプロイ設定修正5

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDwADYm2Vzu9dhlg4f_IhbNQnvhiv0v5Go',
-  authDomain: 'my-shienkin-navi.firebaseapp.com',
-  projectId: 'my-shienkin-navi',
-  storageBucket: 'my-shienkin-navi.appspot.com',
-  messagingSenderId: '1071664114583',
-  appId: '1:1071664114583:web:3489be3309e5e2e920f2d2',
-  measurementId: 'G-75VGFKL6RE',
+  apiKey: 'AIzaSyCKDI3jKAzjPgtmjU820a314yR93J-VjLk',
+  authDomain: 'my-shienkin-navi-67cc2.firebaseapp.com',
+  projectId: 'my-shienkin-navi-67cc2',
+  storageBucket: 'my-shienkin-navi-67cc2.appspot.com',
+  messagingSenderId: '91299918396',
+  appId: '1:91299918396:web:54212a5f8ec0c65bbfc939',
+  measurementId: 'G-J6YHM0550J',
 };
 
 // Initialize Firebase
