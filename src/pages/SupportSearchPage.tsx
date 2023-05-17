7c97bc781c0e4816445bc122442dfc18f5b719a3
Yuma Satake <satous.y@icloud.com>
2023-05-18 04:29:55 +0900

    fix: ページ入れ替え

--- src/pages/SupportSearchPage.tsx
+++ src/pages/SupportSearchPage.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { MainLayout } from '../components/layout/MainLayout';
 
-export const TopPage: FC = () => {
+export const SupportSearchPage: FC = () => {
   return (
     <MainLayout title="支援金検索">
       <div>aaa</div>
