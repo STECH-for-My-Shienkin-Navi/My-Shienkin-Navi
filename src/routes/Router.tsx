c7c4748990096ba9d80ff1d3493038d1122ce9aa
Yuma Satake <satous.y@icloud.com>
2023-05-18 04:30:07 +0900

    feat: common追加

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { TopPage } from '../pages/SupportSearchPage';
-import { SupportSearchPage } from '../pages/TopPage';
+import { TopPage } from '../pages/TopPage';
+import { SupportSearchPage } from '../pages/SupportSearchPage';
 import { SupportListPage } from '../pages/SupportListPage';
 import { SupportDetailPage } from '../pages/SupportDetailPage';
 import { DataTopPage } from '../pages/DataTopPage';
 import { DataSharePage } from '../pages/DataSharePage';
 import { DataReceivePage } from '../pages/DataReceivePage';
 import { ErrorPage } from '../pages/ErrorPage';
+import { Dev } from '../pages/Dev';
 
 export const Router: FC = () => {
   return (
@@ -19,7 +20,8 @@ export const Router: FC = () => {
         <Route path={'/SupportDetail'} element={<SupportDetailPage />} />
         <Route path={'/DataTop'} element={<DataTopPage />} />
         <Route path={'/DataShare'} element={<DataSharePage />} />
-        <Route path={'/DatareceivePage'} element={<DataReceivePage />} />
+        <Route path={'/DataReceivePage'} element={<DataReceivePage />} />
+        <Route path={'/dev'} element={<Dev />} />
         <Route path={'/*'} element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
