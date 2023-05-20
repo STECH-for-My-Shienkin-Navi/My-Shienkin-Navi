3402217b76e389bc3942b4f8f81e053fc7601410
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-20 16:01:09 +0900

    feat: 支援金検索と個人データの共有・受け取りボタンを配置した。

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,10 +1,32 @@
 import { FC } from 'react';
 import { MainLayout } from '../layout/MainLayout';
+import { Card, CardContent, Stack, Typography } from '@mui/material';
+
+const cardStyle = {
+  width: '70%',
+  height: 'auto',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  cursor: 'pointer'
+};
 
 export const TopPage: FC = () => {
   return (
     <MainLayout title="My支援金ナビ">
-      <div>aaa</div>
+      <Stack spacing={2}>
+      <Card variant='outlined' style={cardStyle}>
+        <CardContent>
+          <Typography variant='h5'>支援金検索</Typography>
+          <Typography variant='body1'>あなたのマイナポータルの情報を用いて、ピッタリの支援金を検索できます。</Typography>
+        </CardContent>
+      </Card>
+      <Card variant='outlined' style={cardStyle}>
+        <CardContent>
+          <Typography variant='h5'>個人データの共有・受け取り</Typography>
+          <Typography variant='body1'>支援金の検索や新生児に、他の方（同世帯）の個人データを一時的に共有・受け取ることができます。</Typography>
+        </CardContent>
+      </Card>
+      </Stack>
     </MainLayout>
   );
 };
