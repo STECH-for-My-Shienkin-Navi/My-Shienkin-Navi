cf30dc1a18be3e20d63837c3c4ef6043d9d0191d
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-21 11:39:48 +0900

    feat: DataReceivePage内に、説明を追加した。

--- src/components/pages/DataReceivePage.tsx
+++ src/components/pages/DataReceivePage.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import { MainLayout } from '../layout/MainLayout';
+import { Typography } from '@mui/material';
 
 export const DataReceivePage: FC = () => {
   return (
     <MainLayout title="共有データの受け取り">
-      <div>aaa</div>
+      <Typography variant='body1'>共有されたデータを受け取ります。<br></br>共有リンクを入力して「次へ」を押してください。</Typography>
     </MainLayout>
   );
 };
