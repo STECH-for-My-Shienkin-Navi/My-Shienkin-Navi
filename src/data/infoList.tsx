35f2d2c8d5aa0eac156c0d7481432a673264366c
yukiharada1228 <yukiharada1228@gmail.com>
2023-05-21 19:01:18 +0900

    feat: ダミーデータ追加

---
+++ src/data/infoList.tsx
@@ -0,0 +1,46 @@
+export const infoList = [
+    {
+        title: '支援金名',
+        contents: ['ABC学生給付金'],
+        status: null,
+        isURL: false,
+    },
+    {
+        title: '募集機関',
+        contents: ['ABC支援会'],
+        status: null,
+        isURL: false,
+    },
+    {
+        title: '支援内容',
+        contents: ['対象者に対して、月額5万円を給付'],
+        status: null,
+        isURL: false,
+    },
+    {
+        title: '募集背景',
+        contents: ['未来を作る学生を支援するため'],
+        status: null,
+        isURL: false,
+    },
+    {
+        title: '募集条件 適合チェック',
+        contents: [
+            '世帯所得〇〇万円以下',
+            '直近の成績〇〇以上',
+            '指定する学校に進学',
+        ],
+        status: [
+            true,
+            false,
+            true,
+        ],
+        isURL: false,
+    },
+    {
+        title: '支援金情報URL',
+        contents: ['https://www.〇〇.com'],
+        status: null,
+        isURL: true,
+    },
+];
