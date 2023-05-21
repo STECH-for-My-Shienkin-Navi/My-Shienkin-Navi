df9ac6ffdbba423c9d9e8093b78824c4ea02c939
Yuma Satake <satous.y@icloud.com>
2023-05-21 20:23:02 +0900

    feat: タイポ修正

--- src/components/pages/DataResultPage.tsx
+++ src/components/pages/DataResultPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { MainLayout } from '../layout/MainLayout';
 import { Box, Typography } from '@mui/material';
 import checkImg from '../../assets/check.png';
@@ -8,52 +8,55 @@ import { Col } from '../common/Col';
 import { useNavigate } from 'react-router-dom';
 
 type Props = {
-    title: string;
-    subText: string;
-    isSuccess: boolean;
-    location: string;
-    isShare: number;//0:最初の画面へ戻る、1:支援金の検索へ進む
- }
+  title: string;
+  subText: string;
+  isSuccess: boolean;
+  location: string;
+  isShare: number; //0:最初の画面へ戻る、1:支援金の検索へ進む
+};
 
 export const DataResultPage: FC<Props> = ({ title, subText, isSuccess, location, isShare }) => {
   const navigator = useNavigate();
 
   const navList = [
     {
-      labele: '最初の画面へ戻る',
+      label: '最初の画面へ戻る',
       isSecondary: true,
     },
     {
-      labele: '支援金の検索へ進む',
+      label: '支援金の検索へ進む',
       isPrimary: true,
     },
   ];
 
   return (
     <MainLayout title={title}>
-      <Box sx={{ width: '100%'}}>
-        <Typography align="left">
-          {subText}
-        </Typography>
+      <Box sx={{ width: '100%' }}>
+        <Typography align="left">{subText}</Typography>
         <Box sx={{ textAlign: 'center', mt: 15 }}>
           {isSuccess ? (
-            <img src={checkImg} loading="lazy" style={{ width: 100 }} alt="チェックマークのイラスト" />
-              ) : (
+            <img
+              src={checkImg}
+              loading="lazy"
+              style={{ width: 100 }}
+              alt="チェックマークのイラスト"
+            />
+          ) : (
             <img src={crossImg} loading="lazy" style={{ width: 100 }} alt="バツマークのイラスト" />
           )}
         </Box>
         <Box sx={{ mx: 2, mt: 30 }}>
           <Col spacing={2}>
-                <CommonButton
-                  key={navList[isShare].labele}
-                  isPrimary={navList[isShare].isPrimary}
-                  isSecondary={navList[isShare].isSecondary}
-                  onClick={() => {
-                    navigator(location);
-                  }}
-                >
-                  {navList[isShare].labele}
-                </CommonButton>
+            <CommonButton
+              key={navList[isShare].label}
+              isPrimary={navList[isShare].isPrimary}
+              isSecondary={navList[isShare].isSecondary}
+              onClick={() => {
+                navigator(location);
+              }}
+            >
+              {navList[isShare].label}
+            </CommonButton>
           </Col>
         </Box>
       </Box>
