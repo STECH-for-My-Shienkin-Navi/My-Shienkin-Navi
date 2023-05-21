829369cdc5f655070023717ebb542068a6c23f15
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-21 12:44:28 +0900

    feat: 入力のバリデーションチェックを行うようにした。

--- src/components/pages/DataReceivePage.tsx
+++ src/components/pages/DataReceivePage.tsx
@@ -5,13 +5,27 @@ import { TextField } from '@mui/material';
 
 export const DataReceivePage: FC = () => {
   const [textValue, setTextValue] = useState<string>('');
+  const [textError, setTextError] = useState<boolean>(false);
+
+  const textValidation = (text: string) => {
+    if (text === '') {
+      setTextError(true);
+    } else {
+      setTextError(false);
+    }
+  };
 
   return (
     <MainLayout title="共有データの受け取り">
       <Typography variant='body1' style={{marginBottom: "20px"}}>共有されたデータを受け取ります。<br></br>共有リンクを入力して「次へ」を押してください。</Typography>
       <Stack spacing={1} style={{width: "70%"}}>
         <Typography variant='body1' style={{fontWeight: "bold"}}>共有リンク <span style={{color: "#FF0000"}}>必須</span></Typography>
-        <TextField required id='outlined-required' defaultValue={''} value={textValue} placeholder='入力してください' onChange={(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => { setTextValue(event.target.value)} } />
+        <TextField fullWidth required error={textError} id='outlined-required' defaultValue={''} value={textValue} placeholder='入力してください'
+        onChange={(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+          setTextValue(event.target.value);
+          textValidation(event.target.value);
+        }} />
+        {textError ? <p style={{color: "#FF0000"}}>共有リンクは必須入力です。</p> : <p>        </p>}
       </Stack>
     </MainLayout>
   );
