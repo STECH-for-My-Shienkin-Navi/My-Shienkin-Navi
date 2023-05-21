4eabab74108fd4c8f3a39be740dc893a7041e4f3
yukiharada1228 <yukiharada1228@gmail.com>
2023-05-21 19:09:34 +0900

    fix: alt変更

--- src/components/pages/SupportDetailPage.tsx
+++ src/components/pages/SupportDetailPage.tsx
@@ -53,7 +53,7 @@ export const SupportDetailPage: FC = () => {
     <MainLayout title="支援金の詳細">
       <Box>
         <Box sx={{ textAlign: 'center' }}>
-          <img src={supportImg} loading="lazy" style={{ width: 250 }} alt="家族のイラスト" />
+          <img src={supportImg} loading="lazy" style={{ width: 250 }} alt="支援のイラスト" />
         </Box>
         <Box sx={{ mx: 2, mt: 2 }}>
           <Col spacing={2}>
