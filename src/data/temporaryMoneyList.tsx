c8abbcad6c6790cbaeedff0b64f47b482069b2ec
mochi-yu <mochi.2225.it@gmail.com>
2023-05-23 01:33:55 +0900

    fix: 支援金検索ページの表示内容を修正

--- src/data/temporaryMoneyList.tsx
+++ src/data/temporaryMoneyList.tsx
@@ -1,34 +1,26 @@
 export const temporaryMoneyList = [
     {
-        title: '（公）臨時給付金1',
+        title: '奨学金',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金2',
+        title: '学生ローン',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金3',
+        title: '給付金',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金4',
+        title: '奨学金返還免除制度',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金5',
+        title: '学生生活費貸与制度',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金6',
-        path: '/SupportList',
-    },
-    {
-        title: '（公）臨時給付金7',
-        path: '/SupportList',
-    },
-    {
-        title: '（公）臨時給付金8',
+        title: '特別支援教育費',
         path: '/SupportList',
     },
 ];
