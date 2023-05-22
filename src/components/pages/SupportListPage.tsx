1f3203023785f71b80fd8639f8bf8b966faf0d1f
mochi-yu <mochi.2225.it@gmail.com>
2023-05-23 01:34:16 +0900

    fix: 支援金一覧ページの表示内容を修正

--- src/components/pages/SupportListPage.tsx
+++ src/components/pages/SupportListPage.tsx
@@ -4,6 +4,7 @@ import { CommonButton } from '../common/CommonButton';
 import { CommonCard } from '../common/CommonCard';
 import { useNavigate } from 'react-router-dom';
 import { Stack, Typography, IconButton, } from '@mui/material';
+import { supportMoneyList } from '../../data/supportMoneyList';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import FirstPageIcon from '@mui/icons-material/FirstPage';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
@@ -12,37 +13,13 @@ import LastPageIcon from '@mui/icons-material/LastPage';
 export const SupportListPage: FC = () => {
   const navigate = useNavigate();
 
-  // デモの支援金一覧
-  const navList = [
-    {
-      'title': '支援金名１',
-      'description': '支援金１の説明支援金１の説明支援金１の説明支援金１の説明支援金１の説明支援金１の説明',
-      'link': '/SupportDetail'
-    },
-    {
-      'title': '支援金名２',
-      'description': '支援金２の説明',
-      'link': '/SupportDetail'
-    },
-    {
-      'title': '支援金名３',
-      'description': '支援金３の説明',
-      'link': '/SupportDetail'
-    },
-    {
-      'title': '支援金名４',
-      'description': '支援金４の説明',
-      'link': '/SupportDetail'
-    },
-  ]
-
   return (
     <MainLayout title="奨学金の一覧">
       <Stack sx={{width: '100%'}} spacing={3}>
-        {navList.map((item) => {
+        {supportMoneyList.map((item) => {
           return (
-            <CommonCard title={item.title} onClick={() => navigate(item.link)}>
-              <Typography variant="body1" sx={{marginBottom: '20px'}}>{item.description}</Typography>
+            <CommonCard title={item.title} onClick={() => navigate(item.path)}>
+              <Typography variant="body1" sx={{marginBottom: '20px'}}>{item.contents}</Typography>
               <CommonButton 
                 children="詳細確認・申請"
                 onClick={() => {
