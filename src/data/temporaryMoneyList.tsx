b04bad37818f0c0dc852c3ddec25736e2d7265cc
yukiharada1228 <yukiharada1228@gmail.com>
2023-05-23 00:29:59 +0900

    fix: 一時的なデータのtitleをユニークにすることでkeyエラーを回避

--- src/data/temporaryMoneyList.tsx
+++ src/data/temporaryMoneyList.tsx
@@ -1,34 +1,34 @@
 export const temporaryMoneyList = [
     {
-        title: '（公）臨時給付金',
+        title: '（公）臨時給付金1',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金',
+        title: '（公）臨時給付金2',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金',
+        title: '（公）臨時給付金3',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金',
+        title: '（公）臨時給付金4',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金',
+        title: '（公）臨時給付金5',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金',
+        title: '（公）臨時給付金6',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金',
+        title: '（公）臨時給付金7',
         path: '/SupportList',
     },
     {
-        title: '（公）臨時給付金',
+        title: '（公）臨時給付金8',
         path: '/SupportList',
     },
 ];
