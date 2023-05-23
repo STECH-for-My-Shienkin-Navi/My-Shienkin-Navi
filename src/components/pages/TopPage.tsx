75daf13704db47e79001415b79fa1003176a0b1e
Yuma Satake <satous.y@icloud.com>
2023-05-23 15:12:54 +0900

    fix: トップページのデザイン変更

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import { MainLayout } from '../layout/MainLayout';
 import { Box, Card, CardContent, Stack, Typography } from '@mui/material';
 import { Row } from '../common/Row';
+import { CommonCard } from '../common/CommonCard';
+import checkpaper from '../../assets/checkpaper.png';
+import datashare from '../../assets/datashare.png';
 
 const cardStyle = {
   width: '90%',
@@ -16,12 +19,14 @@ const cardItem = [
   {
     title: '支援金検索',
     description: 'あなたのマイナポータルの情報を用いて、ピッタリの支援金を検索できます。',
+    img: checkpaper,
     path: '/SupportSearch',
   },
   {
     title: '個人データの共有・受け取り',
     description:
       '支援金の検索や新生児に、他の方（同世帯）の個人データを一時的に共有・受け取ることができます。',
+    img: datashare,
     path: '/DataTop',
   },
 ];
@@ -32,24 +37,28 @@ export const TopPage: FC = () => {
   return (
     <MainLayout title="My支援金ナビ">
       <Stack spacing={2}>
+        <Typography sx={{ textAlign: 'center' }}>
+          My支援金Naviでは各種支援金に
+          <br />
+          必要なデータの共有・受取・申請までを
+          <br />
+          ワンストップで行えます
+        </Typography>
         {cardItem.map((item) => {
           return (
-            <Card
+            <CommonCard
               key={item.title}
-              variant="outlined"
-              style={cardStyle}
-              onClick={() => navigate(item.path)}
+              onClick={() => {
+                navigate(item.path);
+              }}
             >
-              <Row>
-                <Box sx={{ bgcolor: 'primary.main', width: '25px' }} />
-                <CardContent>
-                  <Typography variant="h5" style={{ fontWeight: 'bold' }}>
-                    {item.title}
-                  </Typography>
-                  <Typography variant="body1">{item.description}</Typography>
-                </CardContent>
-              </Row>
-            </Card>
+              <Stack alignItems="center">
+                <img src={item.img} loading="lazy" style={{ width: '200px' }} alt={item.title} />
+                <Typography variant="h6" sx={{ textAlign: 'center' }}>
+                  {item.title}
+                </Typography>
+              </Stack>
+            </CommonCard>
           );
         })}
       </Stack>
