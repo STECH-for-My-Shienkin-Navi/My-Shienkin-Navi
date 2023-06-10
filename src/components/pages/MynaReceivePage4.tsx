8c3cfe4cb9f50f4a2ca8a6f0834e3e4cc0b1f7e9
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-06-10 15:45:42 +0900

    muiのtableを導入した。

--- src/components/pages/MynaReceivePage4.tsx
+++ src/components/pages/MynaReceivePage4.tsx
@@ -6,40 +6,85 @@ import { CommonButton } from '../common/CommonButton';
 import { Col } from '../common/Col';
 import { useNavigate } from 'react-router-dom';
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
 export const MynaReceivePage4: FC = () => {
   const navigator = useNavigate();
 
   const navList = [
     {
-      labele: 'データを共有する',
-      path: '/DataShare',
+      label: '支援金の検索へ進む',
       isPrimary: true,
     },
-    {
-      labele: '共有されたデータを受け取る',
-      path: '/DataReceive',
-      isSecondary: true,
-    },
-    {
-      labele: 'データ共有についての説明を見る',
-      path: '/',
-      isText: true,
-    },
-    {
-      labele: '最初の画面へ戻る',
-      path: '/',
-      isText: true,
-    },
   ];
+
+  const listData = [
+    {
+        infoname: "情報1",
+        largeCategory: "大カテゴリ1大カテゴリ1大カテゴリ1",
+        category: "カテゴリ1カテゴリ1カテゴリ1"
+    },
+    {
+        infoname: "情報2",
+        largeCategory: "大カテゴリ2",
+        category: "カテゴリ2"
+    },
+    {
+        infoname: "情報3",
+        largeCategory: "大カテゴリ3",
+        category: "カテゴリ3"
+    }
+  ]
 
   return (
     <MainLayout title="データ取得結果">
       <Box>
         <Typography>
-          選択した以下の情報を取得しました。
+          選択された以下の情報を取得しました。
         </Typography>
-        <Box sx={{ textAlign: 'center' }}>
-          あああああ
+        <Box sx={{ textAlign: 'center', marginTop: "30px" }}>
+        <TableContainer>
+            <Table aria-label="simple table" sx={{ maxWidth: "400px" }}>
+        <TableHead>
+          <TableRow>
+            <TableCell align='left'>情報名</TableCell>
+            <TableCell align="right">大カテゴリ</TableCell>
+            <TableCell align="right">カテゴリ</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {listData.map((data, index) => {
+                return (
+                    <TableRow
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell align="left" sx={{minWidth: "150px"}}>{data.infoname}</TableCell>
+                <TableCell align="right">{data.largeCategory}</TableCell>
+                <TableCell align="right">{data.category}</TableCell>
+              </TableRow>
+                )
+            })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+        </Box>
+        <Box sx={{ mx: 2, mt: 30 }}>
+          <Col spacing={2}>
+            <CommonButton
+              key={navList[0].label}
+              isPrimary={navList[0].isPrimary}
+              onClick={() => {
+                navigator("/DataShare");
+              }}
+            >
+              {navList[0].label}
+            </CommonButton>
+          </Col>
         </Box>
       </Box>
     </MainLayout>
