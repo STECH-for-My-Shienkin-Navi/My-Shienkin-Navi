99a62f45d125dbb5be342f7cdfdbf8d5a5e91e58
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-06-10 14:15:27 +0900

    マイナポータルからの取得画面の４ページ目を作成した。

---
+++ src/components/pages/MynaReceivePage4.tsx
@@ -0,0 +1,68 @@
+import { FC } from 'react';
+import { MainLayout } from '../layout/MainLayout';
+import { Box, Typography } from '@mui/material';
+import familyImg from '../../assets/familImg.png';
+import { CommonButton } from '../common/CommonButton';
+import { Col } from '../common/Col';
+import { useNavigate } from 'react-router-dom';
+
+export const MynaReceivePage4: FC = () => {
+  const navigator = useNavigate();
+
+  const navList = [
+    {
+      labele: 'データを共有する',
+      path: '/DataShare',
+      isPrimary: true,
+    },
+    {
+      labele: '共有されたデータを受け取る',
+      path: '/DataReceive',
+      isSecondary: true,
+    },
+    {
+      labele: 'データ共有についての説明を見る',
+      path: '/',
+      isText: true,
+    },
+    {
+      labele: '最初の画面へ戻る',
+      path: '/',
+      isText: true,
+    },
+  ];
+
+  return (
+    <MainLayout title="データ取得結果">
+      <Box>
+        <Typography>
+          あなたの自己情報を、他の方（同世帯）と一時的に共有することができます。
+          <br />
+          また、他の方に共有されたデータを受け取ることができます。
+        </Typography>
+        <Box sx={{ textAlign: 'center' }}>
+          <img src={familyImg} loading="lazy" style={{ width: 250 }} alt="家族のイラスト" />
+        </Box>
+        <Box sx={{ mx: 2, mt: 2 }}>
+          <Col spacing={2}>
+            {navList.map((item) => {
+              return (
+                <CommonButton
+                  key={item.labele}
+                  isPrimary={item.isPrimary}
+                  isSecondary={item.isSecondary}
+                  isText={item.isText}
+                  onClick={() => {
+                    navigator(item.path);
+                  }}
+                >
+                  {item.labele}
+                </CommonButton>
+              );
+            })}
+          </Col>
+        </Box>
+      </Box>
+    </MainLayout>
+  );
+};
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -8,6 +8,7 @@ import { DataTopPage } from '../components/pages/DataTopPage';
 import { DataSharePage } from '../components/pages/DataSharePage';
 import { DataReceivePage } from '../components/pages/DataReceivePage';
 import { ErrorPage } from '../components/pages/ErrorPage';
+import { MynaReceivePage4 } from '../components/pages/MynaReceivePage4';
 import { Dev } from '../components/pages/Dev';
 
 export const Router: FC = () => {
@@ -21,6 +22,7 @@ export const Router: FC = () => {
         <Route path={'/DataTop'} element={<DataTopPage />} />
         <Route path={'/DataShare'} element={<DataSharePage />} />
         <Route path={'/DataReceive'} element={<DataReceivePage />} />
+        <Route path={'/MynaReceivePage4'} element={<MynaReceivePage4 />} />
         <Route path={'/dev'} element={<Dev />} />
         <Route path={'/*'} element={<ErrorPage />} />
       </Routes>
