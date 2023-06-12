a6977263d3873ceceaf2dc68700cdb08d5e48757
Yuma Satake <satous.y@icloud.com>
2023-06-12 13:42:13 +0900

    feat: 支援金一覧・詳細改修

--- src/components/pages/SupportDetailPage.tsx
+++ src/components/pages/SupportDetailPage.tsx
@@ -1,11 +1,32 @@
 import { FC } from 'react';
-import { Box, Chip, Typography } from '@mui/material';
+import { Box, Chip, Grid, Typography } from '@mui/material';
 import { MainLayout } from '../layout/MainLayout';
 import supportImg from '../../assets/supportImg.png';
 import { Col } from '../common/Col';
 import { useNavigate } from 'react-router-dom';
 import { CommonButton } from '../common/CommonButton';
-import { infoList } from '../../data/infoList';
+import { useRecoilValue } from 'recoil';
+import { SupportDataListState } from '../../data/SupportDataList';
+import { SupportDataType } from '../../types/SupportDataType';
+
+const dispItem = [
+  {
+    title: '支援金名',
+    key: 'name',
+  },
+  {
+    title: '募集団体',
+    key: 'organization',
+  },
+  {
+    title: '支援内容',
+    key: 'content',
+  },
+  {
+    title: '募集背景',
+    key: 'background',
+  },
+];
 
 const OKChip = () => {
   return (
@@ -36,6 +57,10 @@ const NGChip = () => {
 
 export const SupportDetailPage: FC = () => {
   const navigator = useNavigate();
+  const supportDataList = useRecoilValue(SupportDataListState);
+  const selectItemId = 5;
+  const dispContent = supportDataList[selectItemId];
+
   const navList = [
     {
       label: 'この支援金を申請する',
@@ -48,6 +73,7 @@ export const SupportDetailPage: FC = () => {
       isSecondary: true,
     },
   ];
+
   return (
     <MainLayout title="支援金の詳細">
       <Box>
@@ -56,41 +82,52 @@ export const SupportDetailPage: FC = () => {
         </Box>
         <Box sx={{ mx: 2, mt: 2 }}>
           <Col spacing={2}>
-            {infoList.map((item) => {
+            {dispItem.map((item) => {
+              const key = item.key as keyof SupportDataType;
               return (
                 <Box key={item.title}>
                   <Typography variant="h5">{item.title}</Typography>
-                  {item.contents.map((content, index) => {
-                    return (
-                      <Box key={content}>
-                        {item.isURL ? (
-                          <Typography sx={{ pl: 2 }}>
-                            <a href="https://www.〇〇.com">{content}</a>
-                          </Typography>
-                        ) : (
-                          <Box
-                            sx={{
-                              display: 'flex',
-                              alignItems: 'center',
-                              justifyContent: 'space-between',
-                              gap: '50px',
-                            }}
-                          >
-                            <Typography sx={{ pl: 2 }}>{content}</Typography>
-                            {item.status && (item.status[index] ? <OKChip /> : <NGChip />)}
-                          </Box>
-                        )}
-                      </Box>
-                    );
-                  })}
+                  <Typography sx={{ pl: 2, mt: 1 }}>{dispContent[key]}</Typography>
                 </Box>
               );
             })}
+            <Box sx={{ mt: 2 }}>
+              <Typography variant="h5">支援金情報URL</Typography>
+              <Typography sx={{ pl: 2, mt: 1 }}>
+                <a href={dispContent.url}>{dispContent.url}</a>
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h5">募集条件 適合チェック</Typography>
+              <Box sx={{ pl: 2, mt: 1 }}>
+                <Col spacing={0.5}>
+                  {supportDataList[selectItemId].conditions.map((condition, index) => {
+                    return (
+                      <Grid container key={condition}>
+                        <Grid item xs={6}>
+                          <Typography>{condition}</Typography>
+                        </Grid>
+                        <Grid item xs={6} textAlign="center">
+                          {/* {supportDataList[selectItemId].status[index] ? <OKChip /> : <NGChip />} */}
+                          {index !== 2 ? <OKChip /> : <NGChip />}
+                        </Grid>
+                      </Grid>
+                    );
+                  })}
+                </Col>
+              </Box>
+            </Box>
           </Col>
         </Box>
         <Box sx={{ mx: 2, mt: 8 }}>
           <Col spacing={2}>
-            {navList.map((item, index) => {
+            <CommonButton isPrimary isDisabled onClick={() => {}}>
+              この奨学金を申請する
+            </CommonButton>
+            <CommonButton isSecondary onClick={() => navigator('/SupportList')}>
+              支援金の選択肢に戻る
+            </CommonButton>
+            {/* {navList.map((item, index) => {
               return (
                 <CommonButton
                   key={item.label}
@@ -99,18 +136,19 @@ export const SupportDetailPage: FC = () => {
                   onClick={() => {
                     item.path && navigator(item.path);
                   }}
-                  isDisabled={
-                    index === 0
-                      ? !infoList
-                          .map((item) => item.status?.every((status) => status === true))
-                          .every((statusList) => statusList === true || statusList === undefined)
-                      : false
-                  }
+                  isDisabled={true}
+                  // isDisabled={
+                  //   index === 0
+                  //     ? !infoList
+                  //         .map((item) => item.status?.every((status) => status === true))
+                  //         .every((statusList) => statusList === true || statusList === undefined)
+                  //     : false
+                  // }
                 >
                   {item.label}
                 </CommonButton>
               );
-            })}
+            })} */}
           </Col>
         </Box>
       </Box>
